80fb2d7f859b246c4a56854e0db81f1e65305811
rayliu7717 <rayliu2004@gmail.com>
2024-06-22 22:23:48 -0400

    init commit with header, footer and router

---
+++ frontend/src/AppRouter.js
@@ -0,0 +1,16 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route
+  } from 'react-router-dom';
+
+import App from './App';
+import HomeScreen from './screens/HomeScreen';
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<App />}>
+        <Route index={true} path='/' element={<HomeScreen />} /> 
+      </Route>
+    )
+);
