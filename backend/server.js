7c2b729d5b496ccc12765b1f0fdf50b9260e851b
rayliu7717 <rayliu2004@gmail.com>
2024-07-14 07:26:39 -0400

    add bookroute, errorHandler...

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
 import express from 'express'
-import books from './data/books.js';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv'
+import bookRoutes from './routes/bookRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 dotenv.config()
 connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });  
 
-app.get('/api/books', (req, res) => {
-    res.json(books);
-});
+app.use('/api/books', bookRoutes);
 
-app.get('/api/books/:id', (req, res) => {
-    const book = books.find((p) => p._id === req.params.id);
-    res.json(book);
-  });
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
