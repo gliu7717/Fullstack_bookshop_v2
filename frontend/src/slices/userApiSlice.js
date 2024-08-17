82d410497a5b299b5956b7f690d1e5113b1679ab
rayliu7717 <rayliu2004@gmail.com>
2024-08-17 19:09:19 -0400

    update user

--- frontend/src/slices/userApiSlice.js
+++ frontend/src/slices/userApiSlice.js
@@ -48,9 +48,25 @@ export const userApiSlice = apiSlice.injectEndpoints({
         method: 'DELETE',
       }),
     }),
+    getUserDetails: builder.query({
+      query: (id) => ({
+        url: `${USERS_URL}/${id}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/${data.userId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['User'],
+    }),
+
   }),
 });
 
 export const { useLoginMutation,useLogoutMutation, 
   useRegisterMutation,useProfileMutation,
-  useGetUsersQuery,useDeleteUserMutation} = userApiSlice;
+  useGetUsersQuery,useDeleteUserMutation,
+  useUpdateUserMutation, useGetUserDetailsQuery} = userApiSlice;
